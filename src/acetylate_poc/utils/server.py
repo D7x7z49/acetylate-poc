c537609261007e2a712685f3e862430a20df54a8
D7x7z49 <85430783+D7x7z49@users.noreply.github.com>
2024-12-08 19:52:16 +0800

    ruff format

--- src/acetylate_poc/utils/server.py
+++ src/acetylate_poc/utils/server.py
@@ -3,39 +3,31 @@ import asyncio
 import logging
 from logging.handlers import RotatingFileHandler
 
-# 异步文件操作库
-import aiofiles
-
-# 设置日志格式和日志文件
 logger = logging.getLogger("AsyncServer")
 logger.setLevel(logging.INFO)
-# 使用RotatingFileHandler来管理日志文件的大小，并创建滚动日志
+
 handler = RotatingFileHandler("server.log", maxBytes=5 * 1024 * 1024, backupCount=2)
-formatter = logging.Formatter('%(asctime)s - %(levelname)s - %(message)s')
+formatter = logging.Formatter("%(asctime)s - %(levelname)s - %(message)s")
 handler.setFormatter(formatter)
 logger.addHandler(handler)
 
-# 异步日志写入
-async def log_to_file(data):
-    async with aiofiles.open("received_data.txt", "a") as f:
-        await f.write(data + "\n")
 
 async def handle_client(reader, writer):
-    addr = writer.get_extra_info('peername')
+    addr = writer.get_extra_info("peername")
     logger.info(f"Connection from {addr}")
 
     try:
         data = await reader.read(4096)
         if data:
-            message = data.decode('utf-8')
-            logger.info(f"Received: {message} from {addr}")  # 使用日志记录消息
-            await log_to_file(message)  # 将数据写入文件
+            message = data.decode("utf-8")
+            logger.info(f"[+] {message}")
     except Exception as e:
         logger.error(f"Error handling client {addr}: {e}")
     finally:
         logger.info(f"Closing connection to {addr}")
         writer.close()
         await writer.wait_closed()
+
 
 async def main(port):
     host = "0.0.0.0"  # noqa: S104
@@ -46,6 +38,7 @@ async def main(port):
 
     async with server:
         await server.serve_forever()
+
 
 if __name__ == "__main__":
     parser = argparse.ArgumentParser()
